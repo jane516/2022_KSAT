ede21bab771ac9900abe53b51074c63d9c3f7254
jane <jane@teamturing.com>
2022-05-06 21:01:14 +0900

    Init project

---
+++ main.py
@@ -0,0 +1,6 @@
+
+from test import test_function
+
+if __name__ == '__main__':
+    print("hello world")
+    test_function()
---
+++ test.py
@@ -0,0 +1,6 @@
+from sympy import *
+
+
+def test_function():
+    x = symbols("x")
+    print(x+1)
