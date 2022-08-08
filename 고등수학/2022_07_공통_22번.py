597e008a876f5cc6ad0e31f32a456be17432be05
jane516 <rsvp0516@naver.com>
2022-08-08 10:09:46 +0900

    Update 2022_07_공통_22번.py

--- 고등수학/2022_07_공통_22번.py
+++ 고등수학/2022_07_공통_22번.py
@@ -1,17 +1,32 @@
 from sympy import *
+from fractions import Fraction
 
 x, a, b, c = symbols('x a b c')
-fx = a * x * (x - b) * (x - c)
-fx_1 = x * (x - b) * (x - c)
+fx = x * (x - b) * (x - c)
 diff_fx = diff(fx, x)
-diff_fx_1 = diff(fx_1, x)
 gx = diff_fx.subs(x, 0) * x
-gx_1 = diff_fx_1.subs(x, 0) * x
 
-sol1 = solve(fx_1.subs(x, 12) - gx_1.subs(x, 12))
-fx_1 = fx_1.subs(b, sol1[0][b])
-diff_fx_1 = diff_fx_1.subs(b, sol1[0][b])
+sol1 = solve(fx.subs(x, 12) - gx.subs(x, 12))
+fx = fx.subs(b, sol1[0][b])
+gx = gx.subs(b, sol1[0][b])
+diff_fx = diff(fx, x)
+diff_gx = diff(gx, x)
 
-sol2 = solve(fx_1 - diff_fx_1)
-diff_fx_1 = simplify(diff_fx_1)
-print(fx_1, diff_fx_1)
+kx = (fx + gx) / x - diff_fx - diff_gx
+k = solve(kx)[0][x]
+sol3 = solve((diff_fx + diff_gx).subs(x, k))
+fx = a * fx.subs(c, sol3[1])
+gx = a * gx.subs(c, sol3[1])
+hx_1 = fx + gx
+hx_2 = fx - gx
+sol4 = solve(hx_1.subs(x, 3) + Fraction(9, 2))
+fx = fx.subs(a, sol4[0])
+gx = gx.subs(a, sol4[0])
+hx = []
+for i in [6, 11]:
+    if fx.subs(x, i) < 0:
+        hx.append(- 1 * fx.subs(x, i) + gx.subs(x, i))
+    else:
+        hx.append(fx.subs(x, i) + gx.subs(x, i))
+result = k * (hx[0] - hx[1])
+print(simplify(result))
